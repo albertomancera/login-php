de94f6d545b1163f10080c924151bc7f6c42afbf
Alberto Mancera <152221655+albertomancera@users.noreply.github.com>
2025-12-05 17:49:54 +0100

    Add initial HTML structure for the application homepage

---
+++ inicio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de la aplicación</title>
+</head>
+<body>
+    <h2>CRUD a tu aplicación</h2>
+    <p>Bienvenido, <?php echo $_SESSION['nombre'] . " " . $_SESSION['apellidos'] ?></p>
+
+
+</body>
+</html>
